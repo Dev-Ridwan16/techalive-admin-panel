eea3a262312df053c024c3e987c6de3f6055dd50
Thrive Coder <thrivetech793@gmail.com>
2023-09-28 20:18:58 +0100

    Set up the enviroment

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import { AccountForm } from "./components/AccountForm";
+
+function App() {
+  return (
+    <>
+      <AccountForm />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/AccountForm.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const AccountForm = () => {
+  return (
+    <div>
+      <SignupComp />
+    </div>
+  );
+};
+
+export const SignupComp = () => {
+  return (
+    <div>
+      <h1>Hello from Admin Panel</h1>
+    </div>
+  );
+};
