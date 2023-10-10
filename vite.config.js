378bcc73cbb2df8b19817d584a0b35d96e383fb9
Thrive Coder <devridwan1@gmail.com>
2023-10-10 15:25:33 +0100

    added axios as external dependecies

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      // Add "axios" as an external dependency
+      external: ["axios"],
+    },
+  },
   plugins: [react()],
-})
+});
