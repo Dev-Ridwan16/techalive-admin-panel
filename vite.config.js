55103920742974e713f836639408c8a2264872f2
Thrive Coder <devridwan1@gmail.com>
2023-10-10 15:40:55 +0100

    remove node engine

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      // Add "axios" as an external dependency
-      external: ["axios"],
-    },
-  },
   plugins: [react()],
 });
