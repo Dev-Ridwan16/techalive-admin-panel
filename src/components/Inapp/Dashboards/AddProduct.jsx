44038d9df1948c2aa62e961a415cecf2fac5c9b3
Adewole Ridwan <devridwan1@gmail.com>
2025-06-23 00:21:15 +0100

    Update AddProduct.jsx

--- src/components/Inapp/Dashboards/AddProduct.jsx
+++ src/components/Inapp/Dashboards/AddProduct.jsx
@@ -125,7 +125,7 @@ export const AddProduct = () => {
       formData.append('description', productDetails.description)
       try {
         const response = await axios.post(
-          'https://techalive.onrender.com/api/v1/product/add-product',
+          'process.env.SERVER_URL/api/v1/product/add-product',
           formData,
           {
             headers: {
